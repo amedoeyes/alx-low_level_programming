79a2e6694f50d4bd5158c9d67c2baf1d2844536d
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-06 00:06:41 +0300

    add size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints size of various data types
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sizeOfChar = sizeof(char);
+	int sizeOfInt = sizeof(int);
+	int sizeOfLongInt = sizeof(long int);
+	int sizeOfLongLongInt = sizeof(long long int);
+	int sizeOfFloat = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", sizeOfChar);
+	printf("Size of an int: %d byte(s)\n", sizeOfInt);
+	printf("Size of a long int: %d byte(s)\n", sizeOfLongInt);
+	printf("Size of a long long int: %d byte(s)\n", sizeOfLongLongInt);
+	printf("Size of a float: %d byte(s)\n", sizeOfFloat);
+
+	return (0);
+}
