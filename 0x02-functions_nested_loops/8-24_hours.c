1fa56d97ab886196db6c7cb8d4c43e02424f989c
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-10 22:12:03 +0300

    add 24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+#include "main.h"
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		int j;
+
+		for (j = 0; j < 60; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
