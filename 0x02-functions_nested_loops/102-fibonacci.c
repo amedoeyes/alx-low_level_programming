a6200adb794dc31858db3d857cdd21bbb981f5e5
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-11 00:07:06 +0300

    add fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+/**
+ * main - Entry point
+ *
+ * Description: Print the first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	long n1 = 0;
+	long n2 = 1;
+	long r;
+	int i;
+
+	for (i = 0; i < 50; i++)
+	{
+		r = n1 + n2;
+		n1 = n2;
+		n2 = r;
+
+		printf("%ld", r);
+
+		if (i < 49)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
