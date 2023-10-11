9888cdefbd32bebe3594717137e0b0d705d9f4b5
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-11 16:12:39 +0300

    add 0-main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Tests the function `positive_or_negative` with 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
