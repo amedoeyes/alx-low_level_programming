a1ed99bda3fc9c017bbbeba634f7cf41f7231957
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-13 00:34:30 +0300

    fix 10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
 
 		for (j = i; j < size; j++)
@@ -21,7 +21,7 @@ void print_triangle(int size)
 			_putchar(' ');
 		}
 
-		for (j = 0; j < i + 1; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar('#');
 		}
