999971f47aec28cee197d2c478987d2eff23c733
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-13 03:27:30 +0300

    fix 101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,16 @@
 
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (i / 10)
+		print_number(i / 10);
 
-	_putchar('0' + n % 10);
+	_putchar('0' + i % 10);
 }
