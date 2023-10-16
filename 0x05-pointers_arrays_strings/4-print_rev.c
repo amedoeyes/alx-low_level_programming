33f98b6d8598b64f1f93b0498088f7603ad88840
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-16 14:37:23 +0300

    add 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * string_length - returns the length of a string
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+
+int string_length(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * print_rev - print given string in reverse
+ *
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for (i = string_length(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
