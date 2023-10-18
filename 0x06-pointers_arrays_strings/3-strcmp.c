e864122b1417274d02092ff1b12ec068c69be0fd
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-19 00:38:53 +0300

    add 4-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if s1 and s2 are equal
+ *         a negative number if s1 is less than s2
+ *         a positive number if s1 is greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
