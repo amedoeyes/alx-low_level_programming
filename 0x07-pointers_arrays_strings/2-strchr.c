9a63d863ccfccb5b22f45b0ee867671097f35ead
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-24 00:12:44 +0300

    add 2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		s++;
+
+	return (s);
+}
