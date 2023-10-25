d339c30281a4011634a8943dfbafc690da934dfe
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-25 22:25:14 +0300

    fix 6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,16 +15,13 @@ int is_prime_helper(int n, int i)
 	if (n == 2 || n == 3)
 		return (1);
 
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
 	if (i >= n)
 		return (1);
 
-	if (n % i == 0 || n % (i + 2) == 0)
+	if (n % 2 == 0 || n % 3 == 0 || n % i == 0)
 		return (0);
 
-	return (is_prime_helper(n, i + 6));
+	return (is_prime_helper(n, ++i));
 }
 
 /**
@@ -37,5 +34,5 @@ int is_prime_helper(int n, int i)
 
 int is_prime_number(int n)
 {
-	return (is_prime_helper(n, 5));
+	return (is_prime_helper(n, 3));
 }
