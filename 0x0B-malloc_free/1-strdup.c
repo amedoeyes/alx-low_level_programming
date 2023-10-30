b8258647af37b047d6f1d1109abe153da2ab24be
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-10-30 16:23:37 +0200

    add 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicate a string
+ *
+ * @str: string
+ *
+ * Return: pointer to duplicate or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *dup = (char *)malloc(strlen(str) + 1);
+
+	if (dup == NULL)
+		return (NULL);
+
+	strcpy(dup, str);
+
+	return (dup);
+}
