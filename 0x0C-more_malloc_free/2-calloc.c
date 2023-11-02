488b44ed393f2c386244022995369a023c5fbf23
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-02 15:06:10 +0200

    fix betty 2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,15 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * _calloc - allocate memory for an array
+ *
+ *  @nmemb: number of elements
+ *  @size: size of each element
+ *
+ *  Return: pointer to allocated memory
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
