61c60cb62e8316646cebe95d1fb1601e7bce91d6
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-02 15:04:58 +0200

    add 2-calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	return (arr);
+}
