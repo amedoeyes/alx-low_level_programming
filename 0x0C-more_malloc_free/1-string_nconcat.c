4328bca298f1410cd0dcb0342c2a453d20c6151a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-02 14:22:18 +0200

    fix 1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	size_t s1len = strlen(s1);
-	size_t s2len = strlen(s2);
+	size_t s1len = s1 == NULL ? 0 : strlen(s1);
+	size_t s2len = s2 == NULL ? 0 : strlen(s2);
 
 	if (n < s2len)
 		s2len = n;
