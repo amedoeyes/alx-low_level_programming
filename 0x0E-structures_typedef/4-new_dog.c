3d5b412ec38b94794d2963e06f3369b672f7f516
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-06 18:35:41 +0200

    fix 4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,8 +14,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog_t *dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
