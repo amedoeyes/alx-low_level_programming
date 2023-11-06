1a4c6c00d52088f2bd812976bdc5fecaf0faecf8
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-06 18:24:23 +0200

    add dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,9 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+typedef struct dog dog_t;
+
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 
 #endif
