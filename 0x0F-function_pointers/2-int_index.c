faaff038d9ba314beaece37628fcf8c7ae65a259
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-08 06:58:51 +0200

    add 2-int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+/**
+ * int_index - search for an integer in an integer array
+ *
+ * @array: array
+ * @size: size of array
+ * @cmp: comparison callback
+ *
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int index = -1;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (index);
+}
