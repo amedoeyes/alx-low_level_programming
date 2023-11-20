ea1014f66dc126636d0c25a2080a329bfc881e61
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-20 18:51:08 +0200

    fix 6-pop_listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,8 +10,14 @@
 
 int pop_listint(listint_t **head)
 {
-	int n = (*head)->n;
-	listint_t *temp = *head;
+	int n;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = *head;
 
 	*head = (*head)->next;
 	free(temp);
