3fe32e83decf9a5c7258c80ab2da2066e88f75a5
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-20 17:13:34 +0200

    add 5-free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ *
+ * @head: pointer to head of a linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	while (*head)
+	{
+		listint_t *temp = *head;
+
+		*head = temp->next;
+		free(temp);
+	}
+}
