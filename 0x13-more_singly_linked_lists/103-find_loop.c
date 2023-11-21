b4b52777352d416a56a0efbff17f9a89481e9405
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-11-21 14:29:37 +0200

    add 103-find_loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find the cycle in a linked list
+ *
+ * @head: pointer to the head of a linked list
+ *
+ * Return: pointer to the node where the cycle starts
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
