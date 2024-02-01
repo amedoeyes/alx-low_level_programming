9e9dd4845814c3d97bb3ed8aa569499aea9afa3a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-02-01 18:10:33 +0200

    make betty happy

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,9 +2,13 @@
 
 /**
  * key_index - returns index of key in a hash table
+ * @key: key
+ * @size: size of the hash table array
+ *
+ * Return: index of key in the array
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return hash_djb2(key) % size;
+	return (hash_djb2(key) % size);
 }
